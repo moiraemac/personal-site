e74d75ad18f44962353dcc39df5346d53dc9a8cf
David Walker <walk.dv@gmail.com>
2022-11-14 21:17:33 -0500

    styling on em

--- src/frames/education.tsx
+++ src/frames/education.tsx
@@ -9,9 +9,9 @@ function ListFrame() {
             <ul>
                 <li>Master of Applied Science, Industrial Engineering, University of Toronto, 2019</li>
                 <ul>
-                    <li>Thesis:	 Integer and Constraint Programming Approaches to Discretizable Distance Geometry Problems</li>
+                    <li>Thesis:	<a href='https://tspace.library.utoronto.ca/bitstream/1807/98151/1/MacNeil_Moira_201911_MAS_thesis.pdf'>Integer and Constraint Programming Approaches to Discretizable Distance Geometry Problems</a></li>
                 </ul>
-                <li>Bachelor of Science (Honours), Mathematics, Dalhousie University in 2017.</li>
+                <li>Bachelor of Science (Honours), Mathematics, Dalhousie University in 2017</li>
             </ul>
         </div>
     )
--- src/frames/research.tsx
+++ src/frames/research.tsx
@@ -8,8 +8,8 @@ function ListFrame() {
             <h2>Research</h2>
             <h3>Publications</h3>
             <ul>
-                <li>M. MacNeil*, and M. Bodur, "Constraint Programming Approaches to the Discretizable Molecular Distance Geometry Problem", Networks, 79(4): 515-536, 2022.</li>
-                <li>M. MacNeil*, and M. Bodur, "Integer Programming, Constraint Programming, and Hybrid Decomposition Approaches to Discretizable Distance Geometry Problems", INFORMS Journal on Computing, 34(1): 297-314, 2022.</li>
+                <li>M. MacNeil*, and M. Bodur, <a href='https://doi.org/10.1002/net.22068'>"Constraint Programming Approaches to the Discretizable Molecular Distance Geometry Problem"</a>, Networks, 79(4): 515-536, 2022.</li>
+                <li>M. MacNeil*, and M. Bodur, <a href='https://doi.org/10.1287/ijoc.2020.1039'>"Integer Programming, Constraint Programming, and Hybrid Decomposition Approaches to Discretizable Distance Geometry Problems"</a>, INFORMS Journal on Computing, 34(1): 297-314, 2022.</li>
             </ul>
             <h3>Preprints</h3>
             <ul>
